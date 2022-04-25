b42859defaa60cd9ef1d1e1369a498635f646aa6
Lazar Gumarov <gumarov.lazar@gmail.com>
2022-04-25 16:43:11 +0500

    feat: header menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Doc is ready");
+
+  let menuHumburger = document.querySelector(".header-menu .menu-humburger");
+  menuHumburger.addEventListener("click", () => {
+    menuHumburger.classList.toggle("menu-hum__visited");
+
+    let menu = document.querySelector(".header-menu .menu");
+    menu.classList.toggle("menu-hidden");
+  });
+});
