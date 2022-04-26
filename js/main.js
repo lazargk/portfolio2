85cc4bcea15977887bb61ab7659d3706619dadf2
Lazar Gumarov <gumarov.lazar@gmail.com>
2022-04-26 17:20:45 +0500

    feat: excluding footer

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Doc is ready");
 
   let menuHumburger = document.querySelector(".header-menu .menu-humburger");
+
   menuHumburger.addEventListener("click", () => {
     menuHumburger.classList.toggle("menu-hum__visited");
 
     let menu = document.querySelector(".header-menu .menu");
     menu.classList.toggle("menu-hidden");
   });
+
+  
 });
